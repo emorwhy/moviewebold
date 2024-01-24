d6037df2fa7e2ccafad5af08862290b1a345ded3
emorwhy y <emoryntucker@gmail.com>
2024-01-24 18:19:30 +0000

    movie web

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://als-dkfj.onrender.com",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._V_I-Gpchvq823gqplqo2uq9LfX5TfKNUGBihw0Hx2U"
+};
